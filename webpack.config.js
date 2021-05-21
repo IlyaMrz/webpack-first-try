725cf6047bd709761ac0573e1f83f104af886e9e
morozov ilya <knpkaa@gmail.com>
2021-05-21 12:25:44 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './index.html'
+        })
+    ]
+}
